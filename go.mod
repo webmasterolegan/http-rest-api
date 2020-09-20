80cbc334962c5aa7e64037b0905ff660ab543846
Oleg Poliakov <webmasterolegan@gmail.com>
2020-09-20 22:00:58 +1000

    Part 1 complite

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.6.0
-	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/stretchr/testify v1.6.1
 )
--- internal/app/apiserver/apiserver_internal_test.go
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -1,7 +1,17 @@
 package apiserver
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAPIServer_HandleHellow(t *testing.T) {
-
+	s := New(NewConfig())
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/hellow", nil)
+	s.handleHellow().ServeHTTP(rec, req)
+	assert.Equal(t, rec.Body.String(), "Hellow")
 }

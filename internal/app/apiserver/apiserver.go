2158fc9e43cc984a3112b90062e96385ef100efa
Oleg Poliakov <webmasterolegan@gmail.com>
2020-09-20 20:55:32 +1000

    Base cunstruct, start tests

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,55 @@
+package apiserver
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRuter()
+
+	s.logger.Info("Starting API Server")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+	return nil
+}
+
+func (s *APIServer) configureRuter() {
+	s.router.HandleFunc("/hellow", s.handleHellow())
+}
+
+func (s *APIServer) handleHellow() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hellow")
+	}
+}
---
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -0,0 +1,7 @@
+package apiserver
+
+import "testing"
+
+func TestAPIServer_HandleHellow(t *testing.T) {
+
+}
